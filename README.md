961ddd144c50e0c59af06fc01ad7da87f4df3dd0
Mohanasundaram Jemini <mohanasundaramjemini@gmail.com>
2020-06-23 09:33:40 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# torbitassignment
+Install the dependencies
+
+```
+npm install
+```
+
+Make sure that MongoDB is running
+
+```
+mongod
+```
+
+run the server
+
+```
+npm run server
+```
+
+and then in a separate window/tab run the frontend
+
+```
+npm start
+```
+
+The application should be running at [http://localhost:3000/](http://localhost:3000/)
---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import withAuth from "./withAuth";
+import Home from "./Home";
+import Secret from "./Secret";
+import Login from "./Login";
+import Signup from "./Signup";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
